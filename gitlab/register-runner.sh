adfd4703d8f5f0f050fb340f63985fc671bacb93
Magnarox <thevinc@gmx.fr>
2019-02-08 17:46:59 +0100

    FIX add docker privileged on runner

--- gitlab/register-runner.sh
+++ gitlab/register-runner.sh
@@ -5,4 +5,4 @@ echo param 2 : project name
 echo param 3 : gitlab ip
 echo param 4 : register token
 
-sudo docker exec -it $1  gitlab-runner register --non-interactive --executor "docker" --docker-image docker:latest --url "http://$3:9080" --registration-token "$4" --description "$2-runner" --tag-list "$2,docker" --run-untagged --locked="false" --docker-extra-hosts="gitlab.peploleum.com:$3"
+sudo docker exec -it $1  gitlab-runner register --non-interactive --executor "docker" --docker-image docker:latest --url "http://$3:9080" --registration-token "$4" --description "$2-runner" --tag-list "$2,docker" --run-untagged --locked="false" --docker-extra-hosts="gitlab.peploleum.com:$3" --docker-privileged
