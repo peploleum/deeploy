e3d7995b0b7337b0dda2c5d2426082b9527c3395
Magnarox <thevinc@gmx.fr>
2019-02-08 17:41:17 +0100

    gitlab : Add project name in register shell

--- gitlab/register-runner.sh
+++ gitlab/register-runner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo param 1 : gitlab-runner container id
-echo param 2 : gitlab ip
-echo param 3 : register token
+echo param 2 : project name
+echo param 3 : gitlab ip
+echo param 4 : register token
 
-sudo docker exec -it $1  gitlab-runner register --non-interactive --executor "docker" --docker-image docker:latest --url "http://$2:9080" --registration-token "$3" --description "graphy-runner" --tag-list "graphy,docker" --run-untagged --locked="false" --docker-extra-hosts="gitlab.peploleum.com:$2"
+sudo docker exec -it $1  gitlab-runner register --non-interactive --executor "docker" --docker-image docker:latest --url "http://$3:9080" --registration-token "$4" --description "$2-runner" --tag-list "$2,docker" --run-untagged --locked="false" --docker-extra-hosts="gitlab.peploleum.com:$3"
