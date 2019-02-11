fd06db2e2769a3b711b8ccdf13def85058a41761
Cascadeur27 <gregory.jamet@capgemini.com>
2019-02-11 18:23:01 +0100

    Ajout du déploiement kubernetes + readme

---
+++ kubernetes/README.md
@@ -0,0 +1,33 @@
+# deeploy
+
+## Kubernetes
+
+### Prerequisites :  
+* Prepare your system to resolve the namespace corp.capgemini.com (66.28.0.45)
+* copy the following files :
+
+        rbac-kdd.yaml
+        calico.yaml
+        kubernetes-dashboard.yaml
+        service.yaml
+        ClusterRoleBinding.yaml
+
+
+### Download and install kubecfg.p12 :
+
+Download kubecfg.p12 generated without MobaXterm
+Click "Advanced Parameters", manage certificates.
+Import file kubecfg.p12.
+Restart browser.
+
+### Copy token
+
+Copy in file token.tkt, result of the command :
+
+        kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+
+### Access url kubernetes-dashboard
+
+        https://IP du kubernetes master:6443/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy
+
+
---
+++ kubernetes/deployKubernetes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo param 1 : ip du noeud master kubernetes
+
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update && sudo apt-get install docker-ce
+cat > /etc/docker/daemon.json
+{
+	"exec-opts": ["native.cgroupdriver=systemd"],
+	"log-driver": "json-file",
+	"log-opts": {"max-size":"100m" },
+	"storage-driver": "overlay2"
+}
+sudo mkdir -p /etc/systemd/system/docker.service.d
+systemctl daemon-reload && systemctl restart docker
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat > /etc/apt/sources.list.d/kubernetes.list
+	"deb http://apt.kubernetes.io/kubernetes-xenial main"
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo swapoff -a
+sudo kubeadm init --apiserver-advertise-address=$1 --pod-network-cidr=192.168.0.0/16 --feature-gates=CoreDNS=false --node-name kubernetes-master
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f rbac-kdd.yaml
+kubectl apply -f calico.yaml
+kubectl config set-cluster kubernetes --server https://$1:6443 --insecure-skip-tls-verify
+kubectl apply -f kubernetes-dashboard.yaml
+kubectl get pods --all-namespaces
+sudo grep 'client-certificate-data' /etc/kubernetes/admin.conf  | head -n 1 | awk '{print $2}' | base64 -d >> kubecfg.crt
+sudo grep 'client-key-data' /etc/kubernetes/admin.conf  | head -n 1 | awk '{print $2}' | base64 -d >> kubecfg.key
+openssl pkcs12 -export -clcerts -inkey kubecfg.key -in kubecfg.crt -out kubecfg.p12 -name "kubernetes-client"
+kubectl create -f service.yaml
+kubectl create -f ClusterRoleBinding.yaml
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+kubectl get pods --all-namespaces
