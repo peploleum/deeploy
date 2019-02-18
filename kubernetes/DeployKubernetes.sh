9702db6beae4724ca07a33ada73e86ade1f37a94
Cascadeur27 <gregory.jamet@capgemini.com>
2019-02-18 15:13:38 +0000

    Modification des erreurs dans le deploy de kubernetes

--- kubernetes/DeployKubernetes.sh
+++ kubernetes/DeployKubernetes.sh
@@ -8,19 +8,22 @@ sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update && sudo apt-get install docker-ce
-cat > /etc/docker/daemon.json
+cat > /etc/docker/daemon.json << EOF
 {
 	"exec-opts": ["native.cgroupdriver=systemd"],
 	"log-driver": "json-file",
 	"log-opts": {"max-size":"100m" },
 	"storage-driver": "overlay2"
 }
+EOF
+
 sudo mkdir -p /etc/systemd/system/docker.service.d
-systemctl daemon-reload && systemctl restart docker
+sudo systemctl daemon-reload && sudo systemctl restart docker
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat > /etc/apt/sources.list.d/kubernetes.list
-	"deb http://apt.kubernetes.io/kubernetes-xenial main"
+cat > /etc/apt/sources.list.d/kubernetes.list << EOF
+	"deb http://apt.kubernetes.io/ kubernetes-xenial main"
+EOF
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
