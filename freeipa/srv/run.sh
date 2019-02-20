f1478c55fa78e02d19492349a028776f611b6be5
peploleum <peploleum@gmail.com>
2019-02-20 15:33:49 +0100

    unattended freeipa installation options update

--- freeipa/srv/run.sh
+++ freeipa/srv/run.sh
@@ -11,4 +11,4 @@ docker run --rm --name freeipa-server-container \
         -ti -h ipa.peploleum.com \
         -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
         --tmpfs /run --tmpfs /tmp \
-        -v $PWD/srv/data:/data peploleum/freeipa-server -U
+        -v $PWD/srv/data:/data peploleum/freeipa-server -U -r PEPLOLEUM.COM
