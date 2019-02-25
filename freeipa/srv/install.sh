c11abd32fafdd97399c62788a20b54014992bb32
peploleum <peploleum@gmail.com>
2019-02-25 17:52:13 +0100

    added run script (no data purge before running the container)

--- freeipa/srv/install.sh
+++ freeipa/srv/install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+sudo rm -rf data
+mkdir data
+cp conf/ipa-server-install-options data/
+sudo echo '*.*' > data/.gitignore 
+sudo echo '/**' >> data/.gitignore 
 docker run --rm --name freeipa-server-container \
         -e IPA_SERVER_IP=10.65.34.106 -e PASSWORD=adminadmin -p 153:53/udp -p 153:53 \
             -p 180:80 -p 443:443 -p 389:389 -p 636:636 -p 88:88 -p 464:464 \
