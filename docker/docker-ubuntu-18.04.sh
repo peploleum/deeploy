100a9cd39a93a6ac625fa1ad93f6409aa1562b4e
peploleum <peploleum@gmail.com>
2019-02-27 10:34:05 +0000

    fixed typo

--- docker/docker-ubuntu-18.04.sh
+++ docker/docker-ubuntu-18.04.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 #allow apt to use a repository over HTTPS
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 #add Docker's offical GPG key
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #set stable repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 #install docker-ce
