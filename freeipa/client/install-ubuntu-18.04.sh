d323f951d9ccbb8baa0115cc021ad4b55ec90aec
peploleum <peploleum@gmail.com>
2019-02-27 15:58:07 +0000

    finalized freeipa-client enrollment script

--- freeipa/client/install-ubuntu-18.04.sh
+++ freeipa/client/install-ubuntu-18.04.sh
@@ -9,3 +9,5 @@ sudo apt-get install -y freeipa-client
 #add ipa server hostname and IP address to hosts
 echo "10.65.34.192 ipaserver.peploleum.com" | sudo tee -a /etc/hosts
 
+#install freeipa client
+sudo ipa-client-install --hostname=`hostname -f` --mkhomedir --server=ipaserver.peploleum.com --domain peploleum.com --realm PEPLOLEUM.COM
