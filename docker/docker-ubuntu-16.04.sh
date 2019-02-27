7ca6daa2a75845b9686e0153834d6ed38a6e7ebf
peploleum <peploleum@gmail.com>
2019-02-27 10:23:03 +0000

    renamed script for ubuntu 16.04

---
+++ docker/docker-ubuntu-16.04.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update && sudo apt-get install -y docker-ce
+
+sudo cat > /etc/docker/daemon.json << EOF
+{
+"debug": true,
+"ipv6": true,
+"fixed-cidr-v6": "2001:db8:1::/64",
+"hosts": ["unix:///var/run/docker.sock", "tcp://127.0.0.1:2375"]
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+sudo cat > /etc/systemd/system/docker.service.d/docker.conf << EOF
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo sed -i '4i dns-search openstacklocal corp.capgemini.com' /etc/network/interfaces
+sudo sed -i '5i dns-nameservers 66.28.0.45 66.28.0.61 127.0.1.1' /etc/network/interfaces
+
+
