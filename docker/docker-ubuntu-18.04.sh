5c312cef32dc8078ee1a6c3721955d2f48d00e93
peploleum <peploleum@gmail.com>
2019-02-27 10:36:17 +0000

    added missing sudo

--- docker/docker-ubuntu-18.04.sh
+++ docker/docker-ubuntu-18.04.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 #allow apt to use a repository over HTTPS
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 #add Docker's offical GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 #set stable repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
