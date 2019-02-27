459507d56949b6765af8e5231c1fd38d3b49aed1
peplo <peplo@node-01.peploleum.com>
2019-02-27 16:19:53 +0000

    auto mkdir update

--- freeipa/client/install-ubuntu-18.04.sh
+++ freeipa/client/install-ubuntu-18.04.sh
@@ -11,3 +11,13 @@ echo "10.65.34.192 ipaserver.peploleum.com" | sudo tee -a /etc/hosts
 
 #install freeipa client
 sudo ipa-client-install --hostname=`hostname -f` --mkhomedir --server=ipaserver.peploleum.com --domain peploleum.com --realm PEPLOLEUM.COM
+
+#enable mkhomedir
+echo 'Name: activate mkhomedir
+Default: yes
+Priority: 900
+Session-Type: Additional
+Session:
+required pam_mkhomedir.so umask=0022 skel=/etc/skel' | sudo tee /usr/share/pam-configs/mkhomedir
+
+sudo pam-auth-update
