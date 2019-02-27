4cce22746b260051da564dfa2b677b084b58cef5
peploleum <peploleum@gmail.com>
2019-02-27 15:52:49 +0000

    updated freeipa ubuntu package install and prep

--- freeipa/client/install-ubuntu-18.04.sh
+++ freeipa/client/install-ubuntu-18.04.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#install freeipa-client package for ubuntu 18.04 and set hostname according to proper DNS
 sudo apt-get update
 sudo apt-get upgrade
 sudo hostnamectl set-hostname node-01.peploleum.com
+sudo apt-get install -y freeipa-client
+
+#add ipa server hostname and IP address to hosts
+echo "10.65.34.192 ipaserver.peploleum.com" | sudo tee -a /etc/hosts
+
