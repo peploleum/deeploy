730672cb90afe34baea03f70c664ff0a4b30a48f
peploleum <peploleum@gmail.com>
2019-02-27 12:56:06 +0000

    comments

--- docker/docker-ubuntu-18.04.sh
+++ docker/docker-ubuntu-18.04.sh
@@ -8,9 +8,10 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 #set stable repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
+#install docker-ce
 sudo apt-get update && sudo apt-get install -y docker-ce
 
+#set daemon to expose interface on 2375 and enable ipv6 routing to containers
 sudo cat > /etc/docker/daemon.json << EOF
 {
 "debug": true,
@@ -31,10 +32,12 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
+#install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+#add current user to docker group
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
