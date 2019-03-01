fcc43566e3013139c0a49eb0b6f6b874ac773b48
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-01 14:04:30 +0000

    Enfin ça va tout peter

--- openstack/deploy_controller_node.sh
+++ openstack/deploy_controller_node.sh
@@ -68,6 +68,7 @@ openstack role add --project demo --user demo user
 unset OS_AUTH_URL OS_PASSWORD
 openstack --os-auth-url http://$1:5000/v3 --os-default-domain-id default --os-project-domain-name default --os-user-domain-name default --os-project-name admin --os-username admin token issue
 openstack --os-auth-url http://$1:5000/v3 --os-default-domain-id default --os-project-domain-name default --os-user-domain-name default --os-project-name demo --os-username demo token issue
+sudo chmod 777 admin-openrc
 . admin-openrc
 openstack token issue
 
@@ -94,17 +95,16 @@ sudo sed -i '2043i stores = file,http' /etc/glance/glance-api.conf
 sudo sed -i '2044i default_store = file' /etc/glance/glance-api.conf
 sudo sed -i '2045i filesystem_store_datadir = /var/lib/glance/images/' /etc/glance/glance-api.conf
 sudo sed -i '3464i container_formats = ami,ari,aki,bare,ovf,ova,docker' /etc/glance/glance-api.conf
-sudo sed -i '3465i disk_formats = ami,ari,aki,vhd,vhdx,vmdk,raw,qcow2,vdi,iso,ploop.root-tar' /etc/glance/glance-api.conf
-sudo sed -i '3479i auth_uri = http://HOSTNAME_CONTROLLER:5000' /etc/glance/glance-api.conf
-sudo sed -i '3480i auth_url = http://HOSTNAME_CONTROLLER:5000' /etc/glance/glance-api.conf
-sudo sed -i '3481i memcached_servers = HOSTNAME_CONTROLLER:11211' /etc/glance/glance-api.conf
-sudo sed -i '3482i auth_type = password' /etc/glance/glance-api.conf
-sudo sed -i '3483i project_domain_name = default' /etc/glance/glance-api.conf
-sudo sed -i '3484i user_domain_name = default' /etc/glance/glance-api.conf
-sudo sed -i '3485i project_name = service' /etc/glance/glance-api.conf
-sudo sed -i '3486i username = glance' /etc/glance/glance-api.conf
-sudo sed -i '3487i password = root' /etc/glance/glance-api.conf
-sudo sed -i '4494i flavor = keystone' /etc/glance/glance-api.conf
+sudo sed -i '3490i auth_uri = http://HOSTNAME_CONTROLLER:5000' /etc/glance/glance-api.conf
+sudo sed -i '3491i auth_url = http://HOSTNAME_CONTROLLER:5000' /etc/glance/glance-api.conf
+sudo sed -i '3492i memcached_servers = HOSTNAME_CONTROLLER:11211' /etc/glance/glance-api.conf
+sudo sed -i '3493i auth_type = password' /etc/glance/glance-api.conf
+sudo sed -i '3494i project_domain_name = default' /etc/glance/glance-api.conf
+sudo sed -i '3495i user_domain_name = default' /etc/glance/glance-api.conf
+sudo sed -i '3496i project_name = service' /etc/glance/glance-api.conf
+sudo sed -i '3497i username = glance' /etc/glance/glance-api.conf
+sudo sed -i '3498i password = root' /etc/glance/glance-api.conf
+sudo sed -i '4497i flavor = keystone' /etc/glance/glance-api.conf
 
 sudo su -s /bin/sh -c "glance-manage db_sync" glance	
 sudo service glance-registry restart
@@ -142,9 +142,9 @@ openstack endpoint create --region RegionOne placement public http://$1:8778
 openstack endpoint create --region RegionOne placement internal http://$1:8778
 openstack endpoint create --region RegionOne placement admin http://$1:8778
 
-sudo sed -i "s/deb http://archive.ubuntu.com/ubuntu bionic main restricted/deb http://archive.ubuntu.com/ubuntu bionic main universe restricted/g" /etc/apt/source.list
-sudo sed -i "s/deb http://archive.ubuntu.com/ubuntu bionic-security main restricted/deb http://archive.ubuntu.com/ubuntu bionic-security main universe restricted/g" /etc/apt/source.list
-sudo sed -i "s/deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted/deb http://archive.ubuntu.com/ubuntu bionic-updates main universe restricted/g" /etc/apt/source.list
+sudo sed -i "s/deb http://archive.ubuntu.com/ubuntu bionic main restricted/deb http://archive.ubuntu.com/ubuntu bionic main universe restricted/g" /etc/apt/sources.list
+sudo sed -i "s/deb http://archive.ubuntu.com/ubuntu bionic-security main restricted/deb http://archive.ubuntu.com/ubuntu bionic-security main universe restricted/g" /etc/apt/sources.list
+sudo sed -i "s/deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted/deb http://archive.ubuntu.com/ubuntu bionic-updates main universe restricted/g" /etc/apt/sources.list
 sudo apt update
 sudo apt install -y nova-api nova-conductor nova-consoleauth nova-novncproxy nova-scheduler nova-placement-api
 sudo apt install -y rabbitmq-server
@@ -227,17 +227,17 @@ sudo sed -i '5i rpc_backend = rabbit' /etc/neutron/neutron.conf
 sudo sed -i '6i auth_strategy = keystone' /etc/neutron/neutron.conf
 sudo sed -i '7i notify_nova_on_port_status_changes = true' /etc/neutron/neutron.conf
 sudo sed -i '8i notify_nova_on_port_data_changes = true' /etc/neutron/neutron.conf
-sudo sed -i '711,711 s/^/#/' /etc/neutron/neutron.conf
-sudo sed -i '712i connection = mysql+pymysql://neutron:root@localhost/neutron' /etc/neutron/neutron.conf
-sudo sed -i '827i auth_uri = http://HOSTNAME_CONTROLLER:5000' /etc/neutron/neutron.conf
-sudo sed -i '828i auth_url = http://HOSTNAME_CONTROLLER:5000' /etc/neutron/neutron.conf
-sudo sed -i '829i memcached_servers = HOSTNAME_CONTROLLER:11211' /etc/neutron/neutron.conf
-sudo sed -i '830i auth_type = password' /etc/neutron/neutron.conf
-sudo sed -i '831i project_domain_name = default' /etc/neutron/neutron.conf
-sudo sed -i '832i user_domain_name = default' /etc/neutron/neutron.conf
-sudo sed -i '833i project_name = service' /etc/neutron/neutron.conf
-sudo sed -i '834i username = neutron' /etc/neutron/neutron.conf
-sudo sed -i '835i password = root' /etc/neutron/neutron.conf
+sudo sed -i '712,712 s/^/#/' /etc/neutron/neutron.conf
+sudo sed -i '713i connection = mysql+pymysql://neutron:root@localhost/neutron' /etc/neutron/neutron.conf
+sudo sed -i '828i auth_uri = http://HOSTNAME_CONTROLLER:5000' /etc/neutron/neutron.conf
+sudo sed -i '829i auth_url = http://HOSTNAME_CONTROLLER:5000' /etc/neutron/neutron.conf
+sudo sed -i '830i memcached_servers = HOSTNAME_CONTROLLER:11211' /etc/neutron/neutron.conf
+sudo sed -i '831i auth_type = password' /etc/neutron/neutron.conf
+sudo sed -i '832i project_domain_name = default' /etc/neutron/neutron.conf
+sudo sed -i '833i user_domain_name = default' /etc/neutron/neutron.conf
+sudo sed -i '834i project_name = service' /etc/neutron/neutron.conf
+sudo sed -i '835i username = neutron' /etc/neutron/neutron.conf
+sudo sed -i '836i password = root' /etc/neutron/neutron.conf
 sudo sed -i '1117i auth_url = http://HOSTNAME_CONTROLLER:5000' /etc/neutron/neutron.conf
 sudo sed -i '1118i auth_type = password' /etc/neutron/neutron.conf
 sudo sed -i '1119i project_domain_name = default' /etc/neutron/neutron.conf
@@ -246,17 +246,17 @@ sudo sed -i '1121i project_name = service' /etc/neutron/neutron.conf
 sudo sed -i '1122i region_name = RegionOne' /etc/neutron/neutron.conf
 sudo sed -i '1123i username = nova' /etc/neutron/neutron.conf
 sudo sed -i '1124i password = root' /etc/neutron/neutron.conf
-sudo sed -i '1536i rabbit_host = localhost' /etc/neutron/neutron.conf
-sudo sed -i '1537i rabbit_userid = openstack' /etc/neutron/neutron.conf
-sudo sed -i '1538i rabbit_password = root' /etc/neutron/neutron.conf
+sudo sed -i '1539i rabbit_host = localhost' /etc/neutron/neutron.conf
+sudo sed -i '1540i rabbit_userid = openstack' /etc/neutron/neutron.conf
+sudo sed -i '1541i rabbit_password = root' /etc/neutron/neutron.conf
 
 sudo sed -i '129i type_drivers = local,flat,vlan,gre,vxlan,geneve' /etc/neutron/plugins/ml2/ml2_conf.ini
-sudo sed -i '129i tenant_network_types = vxlan' /etc/neutron/plugins/ml2/ml2_conf.ini
-sudo sed -i '129i mechanism_drivers = linuxbridge,l2population' /etc/neutron/plugins/ml2/ml2_conf.ini
-sudo sed -i '129i extension_drivers = port_security' /etc/neutron/plugins/ml2/ml2_conf.ini
-sudo sed -i '181i flat_networks = provider' /etc/neutron/plugins/ml2/ml2_conf.ini
-sudo sed -i '236i vni_ranges = 1:1000' /etc/neutron/plugins/ml2/ml2_conf.ini
-sudo sed -i '253i enable_ipset = true' /etc/neutron/plugins/ml2/ml2_conf.ini
+sudo sed -i '130i tenant_network_types = vxlan' /etc/neutron/plugins/ml2/ml2_conf.ini
+sudo sed -i '131i mechanism_drivers = linuxbridge,l2population' /etc/neutron/plugins/ml2/ml2_conf.ini
+sudo sed -i '132i extension_drivers = port_security' /etc/neutron/plugins/ml2/ml2_conf.ini
+sudo sed -i '183i flat_networks = provider' /etc/neutron/plugins/ml2/ml2_conf.ini
+sudo sed -i '238i vni_ranges = 1:1000' /etc/neutron/plugins/ml2/ml2_conf.ini
+sudo sed -i '255i enable_ipset = true' /etc/neutron/plugins/ml2/ml2_conf.ini
 
 sudo sed -i '147i physical_interface_mappings = provider:PHYSICAL_NETWORK_INTERFACE' /etc/neutron/plugins/ml2/linuxbridge_agent.ini
 sudo sed -i '183i enable_security_group = true' /etc/neutron/plugins/ml2/linuxbridge_agent.ini
@@ -304,22 +304,22 @@ sudo sed -i '67,69 s/#//' /etc/openstack-dashboard/local_settings.py
 sudo sed -i '71,71 s/#//' /etc/openstack-dashboard/local_settings.py
 sudo sed -i '76,76 s/#//' /etc/openstack-dashboard/local_settings.py
 sudo sed -i '98,98 s/#//' /etc/openstack-dashboard/local_settings.py
+sudo sed -i "s/OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'Default'/OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'default'/g" /etc/openstack-dashboard/local_settings.py
 sudo sed -i "s/'LOCATION': '127.0.0.1:11211',/'LOCATION': 'HOSTNAME_CONTROLLER:11211',/g"  /etc/openstack-dashboard/local_settings.py
-sudo sed -i '190i OPENSTACK_HOST = "HOSTNAME_CONTROLLER"' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '191i OPENSTACK_KEYSTONE_URL = "http://%s:5000/v3" % OPENSTACK_HOST' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '192i OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '190,192 s/^/#/' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '193i OPENSTACK_HOST = "HOSTNAME_CONTROLLER"' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '194i OPENSTACK_KEYSTONE_URL = "http://%s:5000/v3" % OPENSTACK_HOST' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '195i OPENSTACK_KEYSTONE_DEFAULT_ROLE = "user"' /etc/openstack-dashboard/local_settings.py
 sudo sed -i "s/'enable_router': True,/'enable_router': False,/g" /etc/openstack-dashboard/local_settings.py
-sudo sed -i '334i '"enable_quotas"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '335i '"enable_ipv6"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '336i '"enable_distributed_router"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '337i '"enable_ha_router"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '338i '"enable_lb"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '339i '"enable_firewall"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '340i '"enable_vpn"': False,' /etc/openstack-dashboard/local_settings.py
-sudo sed -i '341i '"enable_fip_topology_check"': False,' /etc/openstack-dashboard/local_settings.py
+sudo sed -i "s/'enable_quotas': True,/'enable_quotas': False,/g" /etc/openstack-dashboard/local_settings.py
+sudo sed -i "s/'enable_ipv6': True,/'enable_ipv6': False,/g" /etc/openstack-dashboard/local_settings.py
+sudo sed -i '334i \\x27enable_lb\x27: False,' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '335i \\x27enable_firewall\x27: False,' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '346i \\x27enable_vpn\x27: False,' /etc/openstack-dashboard/local_settings.py
+sudo sed -i "s/'enable_fip_topology_check': True,/'enable_fip_topology_check': False,/g" /etc/openstack-dashboard/local_settings.py
 sudo service apache2 reload
 
-sudo sed -i "s/-l 127.0.0.1/IP_CONTROLLER/g"  /etc/memcached.conf
+sudo sed -i "s/-l 127.0.0.1/-l IP_CONTROLLER/g"  /etc/memcached.conf
 sudo service memcached restart
 
 sudo sed -i "s/HOSTNAME_CONTROLLER/$1/g" /etc/glance/glance-api.conf
