6127d02514727d489434ac7812b8ce06163b54aa
peploleum <peploleum@gmail.com>
2019-03-04 16:47:12 +0000

    updated gitlab ldap config scripts

--- gitlab/config/configure-ldap.sh
+++ gitlab/config/configure-ldap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+#sudo is useless in gitlab-ce container
+echo '10.65.34.95 ipaserver.peploleum.com' | tee -a /etc/host
 echo "enabling ldap ... updating gitlab.rb"
-echo "gitlab_rails['ldap_enabled'] = true" | sudo tee -a /etc/gitlab/gitlab.rb
-echo "gitlab_rails['ldap_servers'] = YAML.load_file('/etc/gitlab/freeipa_settings.yml')" | sudo tee -a /etc/gitlab/gitlab.rb
+echo "gitlab_rails['ldap_enabled'] = true" | tee -a /etc/gitlab/gitlab.rb
+echo "gitlab_rails['ldap_servers'] = YAML.load_file('/etc/gitlab/freeipa_settings.yml')" | tee -a /etc/gitlab/gitlab.rb
 echo "restarting gitlab service"
-sudo gitlab-ctl reconfigure
+gitlab-ctl reconfigure
--- gitlab/config/prepare-ldap.sh
+++ gitlab/config/prepare-ldap.sh
@@ -4,4 +4,5 @@
 echo '10.65.34.95 ipaserver.peploleum.com' | sudo tee -a /etc/hosts
 
 docker cp configure-ldap.sh $1:/
+docker cp  freeipa_settings.yml $1:/etc/gitlab/freeipa_settings.yml
 
