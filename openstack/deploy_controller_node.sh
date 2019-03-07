ba184fb230bef85a646d9cba4565a1fa9527f404
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-07 16:36:41 +0000

    Nettoyage pour le déploiement du dashboard

--- openstack/deploy_controller_node.sh
+++ openstack/deploy_controller_node.sh
@@ -365,7 +365,7 @@ sudo sed -i '334i \\x27enable_lb\x27: False,' /etc/openstack-dashboard/local_set
 sudo sed -i '335i \\x27enable_firewall\x27: False,' /etc/openstack-dashboard/local_settings.py
 sudo sed -i '336i \\x27enable_vpn\x27: False,' /etc/openstack-dashboard/local_settings.py
 sudo sed -i "s/'enable_fip_topology_check': True,/'enable_fip_topology_check': False,/g" /etc/openstack-dashboard/local_settings.py
-sudo sed -i '859i SESSION_ENGINE = \\x27django.contrib.sessions.backends.cache\x27' /etc/openstack-dashboard/local_settings.py
+sudo sed -i '859i SESSION_ENGINE = \x27django.contrib.sessions.backends.cache\x27' /etc/openstack-dashboard/local_settings.py
 sudo sed -i "s/HOSTNAME_CONTROLLER/$1/g" /etc/openstack-dashboard/local_settings.py
 sudo service apache2 reload
 
