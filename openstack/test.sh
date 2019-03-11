0fee15e6ee28266794a9fe8455af580d932d9bca
Magnarox <thevinc@gmx.fr>
2019-03-11 17:00:33 +0000

    MAJ du script de validation d'un mdp d'une commande

--- openstack/test.sh
+++ openstack/test.sh
@@ -1,5 +1,21 @@
-#!/usr/bin/expect
+#!/bin/bash
+
+sudo apt install -y expect
+
+## En cas de problème le programme est arrêté au bout de 20 secondes
+set timeout 20
+
+set password root
+
+/usr/bin/expect <<EOD
 
 spawn openstack user create --domain default --password-prompt superman
 #expect "*assword:" { send "$password\n"}
-expect "User Password:" {send "root\n"}
+#expect "User Password:" {send "root\n"}
+
+expect "Password:"
+send "$password\n" 
+expect "Password:"
+send "$password\n" 
+interact
+EOD
