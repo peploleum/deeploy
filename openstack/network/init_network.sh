324e5b0891e1eac9934ad02414942b9eeb4bb5f1
Magnarox <thevinc@gmx.fr>
2019-03-11 15:15:22 +0000

    Init network : Flat

--- openstack/network/init_network.sh
+++ openstack/network/init_network.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This script create a router named router1, public network and subnet based on subnet pool
-echo param 1 : Pool prefix -- ex : 192.168.10.0/24
-echo param 2 : Default prefix length -- ex : 26 
+echo param 1 : Pool prefix -- ex : 192.168.0.0/16
+echo param 2 : Default prefix length -- ex : 18
 
 cd ..
 . admin-openrc
@@ -17,7 +17,7 @@ openstack router remove subnet router1 $(openstack subnet list -c ID -f value)
 openstack router delete router1
 
 openstack router create router1
-openstack network create public --share --external
+openstack network create public --share --external --provider-network-type flat --provider-physical-network provider 
 openstack address scope create --share --ip-version 4 address-scope-ip4
 openstack subnet pool create --address-scope address-scope-ip4 --share --pool-prefix $1 --default-prefix-length $2 subnet-pool-ip4
 openstack subnet create --network public --subnet-pool subnet-pool-ip4 subnet-public
