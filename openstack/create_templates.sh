2347f990432c41ce0a54cb91a1d84340f1160846
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-14 08:26:33 +0000

    Update des IPs

--- openstack/create_templates.sh
+++ openstack/create_templates.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. admin-openrc
+
 #Réseau de dev-virt -- public
 ./create_instance.sh L Ubuntu_18.04_server dev-virt 10.0.0.10 192.168.0.110 core-services
 ./create_instance.sh XXXL Ubuntu_18.04_server dev-virt 10.0.0.31 192.168.0.131 docker-node-01
@@ -11,7 +13,7 @@
 ./create_instance.sh XL Ubuntu_16.04_server dev-virt 10.0.0.22 192.168.0.122 kubernetes-node-02
 
 #Réseau de qualif-virt -- public
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.5 192.168.0.205 kdl-01
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.6 192.168.0.206 kdl-02
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.7 192.168.0.207 kdl-03
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.8 192.168.0.208 kdl-04
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.11 192.168.0.211 kdl-01
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.12 192.168.0.212 kdl-02
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.13 192.168.0.213 kdl-03
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.14 192.168.0.214 kdl-04
