7039d17b33df08c0b64b6de745d3db3656040aae
Magnarox <thevinc@gmx.fr>
2019-03-14 16:04:47 +0000

    Kubespray : FIX Init and Launch

--- ansible/kubespray/init_vm.sh
+++ ansible/kubespray/init_vm.sh
@@ -3,6 +3,11 @@
 cd ~/deeploy/openstack
 . admin-openrc
 
+./remove_instance.sh k8s-ansible 192.168.0.211
+./remove_instance.sh k8s-master 192.168.0.220
+./remove_instance.sh k8s-node1 192.168.0.221
+./remove_instance.sh k8s-node2 192.168.0.222
+
 ./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.11 192.168.0.211 k8s-ansible
 ./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.20 192.168.0.220 k8s-master
 ./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.21 192.168.0.221 k8s-node1
--- ansible/kubespray/run_k8s_install.sh
+++ ansible/kubespray/run_k8s_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd ~/kubespray
 ansible-playbook -i inventory/mycluster/hosts.ini --key-file "~/.ssh/rsa_key.pem" --become --become-user=root cluster.yml
