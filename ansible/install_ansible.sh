f3b828343c074657bb6858d2f3457a925a49e2a6
Magnarox <thevinc@gmx.fr>
2019-03-14 13:27:47 +0000

    Update Ansible Install Script

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -4,4 +4,6 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
-
+sudo apt-get install -y python-netaddr 
+sudo apt-get install -y python-pip
+pip install Jinja2
