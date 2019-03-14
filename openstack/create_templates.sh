7cc6abd2e1259224696c0d8dc0e9a98f3e0d9f33
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-14 10:44:50 +0000

    Openstack : Update template

--- openstack/create_templates.sh
+++ openstack/create_templates.sh
@@ -2,8 +2,23 @@
 
 . admin-openrc
 
+# Cleanup
+./remove_instance.sh core-services 192.168.0.111
+./remove_instance.sh docker-node-01 192.168.0.131
+./remove_instance.sh docker-node-02 192.168.0.132
+./remove_instance.sh docker-node-03 192.168.0.133
+./remove_instance.sh atos-services 192.168.0.150
+./remove_instance.sh kubernetes-master 192.168.0.120
+./remove_instance.sh kubernetes-node-01 192.168.0.121
+./remove_instance.sh kubernetes-node-02 192.168.0.122
+
+./remove_instance.sh kdl-01 192.168.0.171
+./remove_instance.sh kdl-02 192.168.0.172
+./remove_instance.sh kdl-03 192.168.0.173
+./remove_instance.sh kdl-04 192.168.0.174
+
 #Réseau de dev-virt -- public
-./create_instance.sh L Ubuntu_18.04_server dev-virt 10.0.0.10 192.168.0.110 core-services
+./create_instance.sh L Ubuntu_18.04_server dev-virt 10.0.0.11 192.168.0.111 core-services
 ./create_instance.sh XXXL Ubuntu_18.04_server dev-virt 10.0.0.31 192.168.0.131 docker-node-01
 ./create_instance.sh XXL Ubuntu_18.04_server dev-virt 10.0.0.32 192.168.0.132 docker-node-02
 ./create_instance.sh L Ubuntu_18.04_server dev-virt 10.0.0.33 192.168.0.133 docker-node-03
@@ -13,7 +28,7 @@
 ./create_instance.sh XL Ubuntu_16.04_server dev-virt 10.0.0.22 192.168.0.122 kubernetes-node-02
 
 #Réseau de qualif-virt -- public
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.11 192.168.0.211 kdl-01
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.12 192.168.0.212 kdl-02
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.13 192.168.0.213 kdl-03
-./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.14 192.168.0.214 kdl-04
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.11 192.168.0.171 kdl-01
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.12 192.168.0.172 kdl-02
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.13 192.168.0.173 kdl-03
+./create_instance.sh XL Ubuntu_18.04_server qualif-virt 10.0.10.14 192.168.0.174 kdl-04
