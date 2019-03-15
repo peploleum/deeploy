b33178596bb521e398ccabfbcca62249ef1a5685
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-15 14:37:45 +0000

    Update kubespray init file

--- ansible/kubespray/init_vm.sh
+++ ansible/kubespray/init_vm.sh
@@ -12,3 +12,9 @@ cd ~/deeploy/openstack
 ./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.20 192.168.0.220 k8s-master ~/deeploy/ansible/kubespray/cloud-config-k8s.yml
 ./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.21 192.168.0.221 k8s-node1 ~/deeploy/ansible/kubespray/cloud-config-k8s.yml
 ./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.22 192.168.0.222 k8s-node2 ~/deeploy/ansible/kubespray/cloud-config-k8s.yml
+
+openstack server add security group k8s-ansible kubernetes
+openstack server add security group k8s-master kubernetes
+openstack server add security group k8s-node1 kubernetes
+openstack server add security group k8s-node2 kubernetes
+
