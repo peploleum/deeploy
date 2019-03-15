f9c09f9c4551a619c2cbce14f7598ca186d009bb
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-15 12:37:10 +0000

    Kubespray : Auto init openstack VMs

--- ansible/kubespray/init_vm.sh
+++ ansible/kubespray/init_vm.sh
@@ -8,7 +8,7 @@ cd ~/deeploy/openstack
 ./remove_instance.sh k8s-node1 192.168.0.221
 ./remove_instance.sh k8s-node2 192.168.0.222
 
-./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.11 192.168.0.211 k8s-ansible
-./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.20 192.168.0.220 k8s-master
-./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.21 192.168.0.221 k8s-node1
-./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.22 192.168.0.222 k8s-node2
+./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.11 192.168.0.211 k8s-ansible ~/deeploy/ansible/kubespray/cloud-config-manager.yml
+./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.20 192.168.0.220 k8s-master ~/deeploy/ansible/kubespray/cloud-config-k8s.yml
+./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.21 192.168.0.221 k8s-node1 ~/deeploy/ansible/kubespray/cloud-config-k8s.yml
+./create_instance.sh M Ubuntu_18.04_server sandbox 12.12.12.22 192.168.0.222 k8s-node2 ~/deeploy/ansible/kubespray/cloud-config-k8s.yml
