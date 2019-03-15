d26bc01eded5a94415ff4ae92f48a3d817b6c822
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-15 14:55:38 +0000

    Update kubespray init

--- ansible/kubespray/run_k8s_install.sh
+++ ansible/kubespray/run_k8s_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cd ../../openstack
+. admin-openrc
+
 cd ~/kubespray
 ansible-playbook -i inventory/mycluster/hosts.ini --key-file "~/.ssh/rsa_key.pem" --become --become-user=root cluster.yml
