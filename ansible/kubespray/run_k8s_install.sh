a77f4c911baf70b7f97bbf1faa74da04246bad87
Magnarox <thevinc@gmx.fr>
2019-03-15 15:32:24 +0000

    FIX RSA key

--- ansible/kubespray/run_k8s_install.sh
+++ ansible/kubespray/run_k8s_install.sh
@@ -4,4 +4,4 @@ cd ../../openstack
 . admin-openrc
 
 cd ~/kubespray
-ansible-playbook -i inventory/mycluster/hosts.ini --key-file "~/.ssh/rsa_key.pem" --become --become-user=root cluster.yml
+ansible-playbook -i inventory/mycluster/hosts.ini --key-file "~/rsa_key.pem" --become --become-user=root cluster.yml
