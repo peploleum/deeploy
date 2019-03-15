89d8f2a8cc9b75fcf59d097f48c04cd131c31830
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-15 11:19:33 +0000

    Update create_instance script

--- openstack/create_instance.sh
+++ openstack/create_instance.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-echo param 1 : Flavor name -- ex : Small
-echo param 2 : Image name -- ex : Ubuntu-16.04
-echo param 3 : Network name -- ex : sandbox
-echo param 4 : Private Instance IP -- ex : 10.0.10.113
-echo param 5 : Public Floating IP -- ex : 192.168.0.113
-echo param 6 : Instance name -- ex : instance04
+if [ $# -lt 6 ] || [ $# -gt 7 ] ; then
+  echo "Usage: create_instance.sh FLAVOR IMAGE NETWORK IP_PRIVATE IP_PUBLIC INSTANCE_NAME USER_DATA[optionnal]"
+  echo
+  echo FLAVOR must meet the following:
+  echo "  • be defined in openstack" 
+  echo
+  echo IMAGE must meet the following:
+  echo "  • be defined in openstack"
+  echo
+  echo NETWORK must meet the following:
+  echo "  • be defined in openstack"
+  echo
+  echo IP_PRIVATE must meet the following:
+  echo "  • valid IPv4"
+  echo "  • IP include in the NETWORK IP Range"
+  echo
+  echo IP_PUBLIC must meet the following:
+  echo "  • valid IPv4"
+  echo "  • IP include in the public openstack network IP Range"
+  echo
+  echo INSTANCE_NAME must meet the following:
+  echo "  • valid hostname"
+  echo
+  echo USER_DATA is optionnal:
+  echo "  • valid user data file"
+  echo
+  echo "Examples:"
+  echo "  • $0 S Ubuntu_18.04_server sandbox 12.12.12.118 192.168.0.218 my_instance" 
+  echo "  • $0 S Ubuntu_18.04_server sandbox 12.12.12.118 192.168.0.218 my_instance my_user_data.txt"
+  exit 1
+fi
 
 KEY_NAME=rsa_key
 
@@ -13,7 +38,19 @@ KEY_NAME=rsa_key
 openstack port create --network $3 --fixed-ip subnet=subnet-$3,ip-address=$4 --device-owner compute:nova --security-group openstack port-$6
 
 #Create Instance
-openstack server create --flavor $1 --image $2 --key-name $KEY_NAME --port port-$6 $6
+nb_param=$#
+case $nb_param in
+  6)
+    openstack server create --flavor $1 --image $2 --key-name $KEY_NAME --port port-$6 $6
+    ;;
+  7)
+    openstack server create --flavor $1 --image $2 --key-name $KEY_NAME --user-data $7 --port port-$6 $6
+    ;;
+  *)
+    echo "Bad param"
+    exit 1
+    ;;
+esac
 
 #Add floating-ip
 openstack floating ip create public --floating-ip-address $5 --port port-$6
