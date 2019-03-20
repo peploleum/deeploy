049dde1aac11efdf74004616846ec827de2f3a22
peploleum <peploleum@gmail.com>
2019-03-20 18:01:19 +0100

    fixed typo

--- jira/install-ubuntu-18.04.sh
+++ jira/install-ubuntu-18.04.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
 #choose option 2 (custom) for the most control.
 INSTALL_TYPE=2
 #this is where Jira will be installed.
 DESTINATION_DIRECTORY=/usr/share/jira
 #this is where Jira data like logs, search indexes and files will be stored.
 HOME_DIRECTORY=/usr/share/jira/home
-set -x
 mkdir dist && cd dist
 mkdir $DESTINATION_DIRECTORY
 wget https://product-downloads.atlassian.com/software/jira/downloads/atlassian-jira-software-8.0.2-x64.bin
