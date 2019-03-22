b46cb9821e9bb1fcdff68edcdfb70dcdd5accc7f
Cascadeur27 <gregory.jamet@capgemini.com>
2019-03-22 14:29:49 +0000

    Ajout d'une première version du fichier de config

--- webmail/install_redmail.sh
+++ webmail/install_redmail.sh
@@ -14,8 +14,9 @@ fi
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 $(hostname -f).$1 localhost/g" /etc/hosts
 cd /home/ubuntu/deeploy/webmail
 tar xjf iRedMail-0.9.9.tar.bz2
+cp config /iRedMail-0.9.9/
 cd /home/ubuntu/deeploy/webmail/iRedMail-0.9.9/
 sudo chmod +x iRedMail.sh
 sudo ./iRedMail.sh
-cd ..
-sudo rm -r iRedMail-0.9.9
+#cd ..
+#sudo rm -r iRedMail-0.9.9
