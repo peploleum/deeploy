eafe2924487c4a6e0a7956fbf14ab554c95defee
Magnarox <thevinc@gmx.fr>
2019-03-27 08:25:01 +0100

    Update init vm script

--- ansible/nexus/vm/init_vm.sh
+++ ansible/nexus/vm/init_vm.sh
@@ -3,7 +3,7 @@
 # This script init 4 VM to deploy Kubernetes. Please check kubespray.conf file before runnig this script.
 
 # Load variables
-. nexus.conf 
+. nexus.conf
 . $DEEPLOY_PATH/openstack/admin-openrc
 
 # Remove Instances
@@ -19,14 +19,14 @@ then
         echo "Openstack nexus security group already exist."
 else
 	openstack security group create nexus
-#	openstack security group rule create kubernetes --protocol udp --dst-port 1:65535
-#	openstack security group rule create kubernetes --protocol tcp --dst-port 1:65535
-#	openstack security group rule create kubernetes --protocol icmp
+	openstack security group rule create nexus --protocol tcp --dst-port 8081
+#	openstack security group rule create nexus --protocol tcp --dst-port 1:65535
+#	openstack security group rule create nexus --protocol icmp
 fi
 
 # Create Instances
-$DEEPLOY_PATH/openstack/create_instance.sh $ANSIBLE_FLAVOR $OPENSTACK_IMAGE $PRIVATE_NETWORK_NAME  $ANSIBLE_PRIVATE_IP $ANSIBLE_PUBLIC_IP $ANSIBLE_NAME
+$DEEPLOY_PATH/openstack/create_instance.sh $ANSIBLE_FLAVOR $OPENSTACK_IMAGE $PRIVATE_NETWORK_NAME  $ANSIBLE_PRIVATE_IP $ANSIBLE_PUBLIC_IP $ANSIBLE_NAME $DEEPLOY_PATH/ansible/nexus/vm/cloud-config-manager.yml
 #openstack server add security group $ANSIBLE_NAME nexus
 $DEEPLOY_PATH/openstack/create_instance.sh $MASTER_FLAVOR  $OPENSTACK_IMAGE $PRIVATE_NETWORK_NAME  $MASTER_PRIVATE_IP $MASTER_PUBLIC_IP $MASTER_NAME
-#openstack server add security group $ANSIBLE_NAME nexus
+#openstack server add security group $MASTER_NAME nexus
 
