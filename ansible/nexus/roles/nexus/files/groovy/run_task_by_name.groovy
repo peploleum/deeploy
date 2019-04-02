2d16d2f68dd6cf084e06f83ae7d24f12ebb16855
Magnarox <thevinc@gmx.fr>
2019-04-02 17:49:18 +0200

    Nexus : FIX run task script

--- ansible/nexus/roles/nexus/files/groovy/run_task_by_name.groovy
+++ ansible/nexus/roles/nexus/files/groovy/run_task_by_name.groovy
@@ -1,7 +1,10 @@
 import groovy.json.JsonSlurper
 import org.sonatype.nexus.scheduling.TaskInfo
+import org.sonatype.nexus.scheduling.TaskScheduler
 
 parsed_args = new JsonSlurper().parseText(args)
+
+TaskScheduler taskScheduler = container.lookup(TaskScheduler.class.getName())
 
 TaskInfo existingTask = taskScheduler.listsTasks().find { TaskInfo taskInfo ->
   taskInfo.name == parsed_args.name
@@ -9,6 +12,7 @@ TaskInfo existingTask = taskScheduler.listsTasks().find { TaskInfo taskInfo ->
 
 if (existingTask) {
   existingTask.runNow()
+  existingTask.getCurrentState();
 }
 else
   throw new RuntimeException("Unknown task : " + parsed_args.name)
