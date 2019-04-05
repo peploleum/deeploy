e81cb8d641589f11240819e529dde77eb7c9515d
Magnarox <thevinc@gmx.fr>
2019-04-05 16:05:51 +0000

    Update nexus-populate.sh

--- ansible/nexus/vm/nexus-populate.sh
+++ ansible/nexus/vm/nexus-populate.sh
@@ -22,4 +22,17 @@ docker pull ${nexusIp}:${nexusDockerPort}/beats/packetbeat-oss:6.6.2
 
 docker pull ${nexusIp}:${nexusDockerPort}/sonarqube:7.6-community
 
+docker pull ${nexusIp}:${nexusDockerPort}/gitlab/gitlab-ce:11.9.1-ce.0
+docker pull ${nexusIp}:${nexusDockerPort}/gitlab/gitlab-runner:v11.9.1
+
+docker pull ${nexusIp}:${nexusDockerPort}/sonatype/nexus3:3.15.2
+
+docker pull ${nexusIp}:${nexusDockerPort}/mongo:3.6.11-xenial
+docker pull ${nexusIp}:${nexusDockerPort}/cassandra:3.11.4
+docker pull ${nexusIp}:${nexusDockerPort}/redis:4.0.14
+docker pull ${nexusIp}:${nexusDockerPort}/mariadb:10.2.23
+docker pull ${nexusIp}:${nexusDockerPort}/postgres:9.6.12
+## Janusgraph (pas de repos docker officiel)
+#docker pull ${nexusIp}:${nexusDockerPort}/janusgraph:0.3.1
+
 
