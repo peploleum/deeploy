376e556b867011c4af75d4dbe90661b6959cb0a5
peploleum <peploleum@gmail.com>
2019-05-06 16:30:30 +0200

    added ARGS to free-ipa centos enrollment script

--- freeipa/client/install-centos7-1810.sh
+++ freeipa/client/install-centos7-1810.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 if [ $# -lt 6 ]; then
-  echo "Usage: $0 IPA_SERVER_IP IPA_SERVER_FQDN IPA_DOMAIN_NAME IPA_REALM IPA_PRINCIPAL IPA_PASSWORD"
+  echo "Usage: $0 IPA_SERVER_IP IPA_SERVER_FQDN IPA_DOMAIN_NAME IPA_REALM IPA_PRINCIPAL IPA_PASSWORD [ARGS]"
   echo
   echo IPA_SERVER_IP must be:
   echo "  • valid IP of running IPA server"
@@ -21,11 +21,14 @@ if [ $# -lt 6 ]; then
   echo IPA_PASSWORD must meet the following:
   echo "  • admin password"
   echo
+  echo [ARGS] must meet the following:
+  echo "  • args that will be appended to the command line"
+  echo
   echo  echo "Examples:"
   echo "  • $0 10.0.0.1 ipaserver.peploleum.com peploleum.com PEPLOLEUM.COM admin adminadmin"
   exit 1
 fi
-
+echo "ARGS are ${@:7}"
 #install freeipa-client package for ubuntu 18.04 and set hostname according to proper DNS
 sudo yum -y update
 sudo hostnamectl set-hostname $(hostname -s)
@@ -35,6 +38,7 @@ sudo yum install -y freeipa-client
 echo "$1 $2" | sudo tee -a /etc/hosts
 echo "127.0.1.1 $(hostname -s).$3 $(hostname -s)" | sudo tee -a /etc/hosts
 
+
 #install freeipa client
-sudo ipa-client-install --hostname=`hostname -f` --mkhomedir --server=$2 --domain=$3 --realm=$4 --principal=$5 --password=$6 -U
+sudo ipa-client-install --hostname=`hostname -f` --mkhomedir --server=$2 --domain=$3 --realm=$4 --principal=$5 --password=$6 -U "${@:7}"
 
