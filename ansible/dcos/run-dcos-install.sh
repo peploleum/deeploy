825dc96278fb8359773c45d28768d516b4ae087d
peploleum <peploleum@gmail.com>
2019-05-06 17:25:34 +0200

    updated variables loading path to match repo path

--- ansible/dcos/run-dcos-install.sh
+++ ansible/dcos/run-dcos-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Load variables
-. /home/temp/deeploy/dcos/dcos.conf
-. $DEEPLOY_PATH/openstack/admin-openrc
+. /home/temp/deeploy/ansible/dcos/dcos.conf
+. /home/temp/deeploy/openstack/admin-openrc
 
 cd ~/dcos
 ansible-playbook -i inventory.ini --key-file "/home/temp/rsa_key.pem" dcos.yml
