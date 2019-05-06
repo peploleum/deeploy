ecd789350e5690a16e0e7aa735df3090cc70abf8
peploleum <peploleum@gmail.com>
2019-05-06 12:25:50 +0200

    added group vars ansible config file. update prepare script

--- ansible/dcos/prepare-manager-vm.sh
+++ ansible/dcos/prepare-manager-vm.sh
@@ -24,3 +24,9 @@ sed -i -e "s/##BOOSTRAP_FQDN##/$BOOSTRAP_FQDN/g" inventory.ini
 sed -i -e "s/##AGENT_FQDN##/$AGENT_FQDN/g" inventory.ini
 sed -i -e "s/##MASTER_FQDN##/$MASTER_FQDN/g" inventory.ini
 
+# Copy ``vars config file``
+cp -rfp /home/temp/deeploy/ansible/dcos/ansible/dcos.yaml.template group_vars/all/dcos.yaml
+
+# Update Ansible config file
+sed -i -e "s/##BOOSTRAP_FQDN##/$BOOSTRAP_FQDN/g" group_vars/all/dcos.yaml
+sed -i -e "s/##MASTER_PUBLIC_IP##/$MASTER_PUBLIC_IP/g" group_vars/all/dcos.yaml
