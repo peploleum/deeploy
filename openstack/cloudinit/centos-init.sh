6d60b2a2e4f776ddccd4db1f9db2948ad270a7de
Magnarox <magnarox06@gmail.com>
2019-05-07 14:27:00 +0200

    cloud-init centos

---
+++ openstack/cloudinit/centos-init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Create /etc/environment file
+sudo tee -a /etc/environment <<EOF
+http_proxy="http://proxyserver.peploleum.com:3128/"
+https_proxy="https://proxyserver.peploleum.com:3128/"
+ftp_proxy="ftp://proxyserver.peploleum.com:3128/"
+no_proxy=".peploleum.com"
+EOF
