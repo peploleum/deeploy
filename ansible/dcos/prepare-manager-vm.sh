20c7ed09179b82d6866e327ea0a4ed996d47c0d1
peploleum <peploleum@gmail.com>
2019-05-13 18:29:11 +0200

    switched to ip-detect content settings in config to match dcos ansible documentation

--- ansible/dcos/prepare-manager-vm.sh
+++ ansible/dcos/prepare-manager-vm.sh
@@ -32,8 +32,6 @@ sed -i -e "s/##PRIVATE_AGENT_PRIVATE_IP##/$PRIVATE_AGENT_PRIVATE_IP/g" inventory
 
 # Copy ``vars config file``
 cp -rfp /home/temp/deeploy/ansible/dcos/ansible/dcos.yaml.template group_vars/all/dcos.yaml
-mkdir -p group_vars/all/genconf
-cp -rfp /home/temp/deeploy/ansible/dcos/ansible/custom-ip-detect group_vars/all/genconf/ip-detect
 # copy ansible.cfg with hash_behaviour set to merge
 cp -rfp /home/temp/deeploy/ansible/dcos/ansible/ansible.cfg ansible.cfg
 
