82e1bdbf586b6dcc4e14b57d9f02b0719fdba9f8
peploleum <peploleum@gmail.com>
2019-05-13 11:02:14 +0200

    switched to private IPs to try zk resolution fix

--- ansible/dcos/prepare-manager-vm.sh
+++ ansible/dcos/prepare-manager-vm.sh
@@ -26,9 +26,9 @@ sed -i -e "s/##PRIVATE_AGENT_FQDN##/$PRIVATE_AGENT_FQDN/g" inventory.ini
 sed -i -e "s/##PUBLIC_AGENT_FQDN##/$PUBLIC_AGENT_FQDN/g" inventory.ini
 sed -i -e "s/##MASTER_FQDN##/$MASTER_FQDN/g" inventory.ini
 sed -i -e "s/##BOOTSTRAP_PUBLIC_IP##/$BOOTSTRAP_PUBLIC_IP/g" inventory.ini
-sed -i -e "s/##MASTER_PUBLIC_IP##/$MASTER_PUBLIC_IP/g" inventory.ini
-sed -i -e "s/##PUBLIC_AGENT_PUBLIC_IP##/$PUBLIC_AGENT_PUBLIC_IP/g" inventory.ini
-sed -i -e "s/##PRIVATE_AGENT_PUBLIC_IP##/$PRIVATE_AGENT_PUBLIC_IP/g" inventory.ini
+sed -i -e "s/##MASTER_PRIVATE_IP##/$MASTER_PRIVATE_IP/g" inventory.ini
+sed -i -e "s/##PUBLIC_AGENT_PRIVATE_IP##/$PUBLIC_AGENT_PRIVATE_IP/g" inventory.ini
+sed -i -e "s/##PRIVATE_AGENT_PRIVATE_IP##/$PRIVATE_AGENT_PRIVATE_IP/g" inventory.ini
 
 # Copy ``vars config file``
 cp -rfp /home/temp/deeploy/ansible/dcos/ansible/dcos.yaml.template group_vars/all/dcos.yaml
