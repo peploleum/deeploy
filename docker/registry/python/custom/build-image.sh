ac9d4fa40b7c16f58fc42835b3f02936783b3da9
peploleum <peploleum@gmail.com>
2019-05-20 12:09:25 +0200

    fixed typo

--- docker/registry/python/custom/build-image.sh
+++ docker/registry/python/custom/build-image.sh
@@ -4,7 +4,8 @@
 . ../nexus.conf
 
 # change base image here  (3.6.8-jessie | 3.6.8-alpine3.8)
-export IMAGE=3.6.8-jessie
+export VERSION=3.6.8-jessie
+export IMAGE=python:$VERSION
 export SOURCE=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PULL_PORT/$IMAGE
 export TARGET=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT/$IMAGE
 
