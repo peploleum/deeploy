67c19c210d72238cdd68cbec571eb79484e2b04e
peploleum <peploleum@gmail.com>
2019-05-20 11:02:26 +0200

    fixed jessie build

--- docker/registry/python/jessie/build-image.sh
+++ docker/registry/python/jessie/build-image.sh
@@ -3,8 +3,8 @@
 . ../../docker.conf
 . ../nexus.conf
 
-export VERSION=3.6.8
-export IMAGE=python:$VERSION-jessie
+export VERSION=3.6.8-jessie
+export IMAGE=python:$VERSION
 export SOURCE=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PULL_PORT/$IMAGE
 export TARGET=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT/$IMAGE
 
@@ -14,9 +14,13 @@ sed -i -e "s/##NEXUS_PORT##/$NEXUS_PORT/g" pip.conf
 
 echo $SOURCE
 echo $TARGET
-docker build -t $IMAGE --build-arg IMAGE=$IMAGE --build-arg NEXUS_HOST=$NEXUS_HOST --build-arg NEXUS_PORT=$NEXUS_PORT --no-cache .
-docker tag $IMAGE $TARGET
-docker login $DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT -u $DOCKER_PRIVATE_REGISTRY_USER -p $DOCKER_PRIVATE_REGISTRY_PASSWORD
-
-echo pushing $TARGET
-docker push $TARGET
+(docker build -t $IMAGE --build-arg IMAGE=$IMAGE --build-arg NEXUS_HOST=$NEXUS_HOST --build-arg NEXUS_PORT=$NEXUS_PORT --no-cache .)
+if [ "$?" == 0 ] ; then
+  docker tag $IMAGE $TARGET
+  docker login $DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT -u $DOCKER_PRIVATE_REGISTRY_USER -p $DOCKER_PRIVATE_REGISTRY_PASSWORD
+  echo pushing $TARGET
+  docker push $TARGET
+else
+  echo "failed to build image"
+	exit
+fi
