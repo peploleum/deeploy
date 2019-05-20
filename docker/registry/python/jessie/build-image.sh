3382eebac9a672d4949ac053b6704e7a78e7888c
peploleum <peploleum@gmail.com>
2019-05-20 10:15:57 +0200

    fixed scripts to customize versions

--- docker/registry/python/jessie/build-image.sh
+++ docker/registry/python/jessie/build-image.sh
@@ -3,7 +3,8 @@
 . ../../docker.conf
 . ../nexus.conf
 
-export IMAGE=python:3.6.8-jessie
+export VERSION=3.6.8
+export IMAGE=python:$VERSION-jessie
 export SOURCE=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PULL_PORT/$IMAGE
 export TARGET=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT/$IMAGE
 
@@ -13,7 +14,7 @@ sed -i -e "s/##NEXUS_PORT##/$NEXUS_PORT/g" pip.conf
 
 echo $SOURCE
 echo $TARGET
-docker build -t $IMAGE --build-arg IMAGE=$SOURCE --build-arg NEXUS_HOST=$NEXUS_HOST --build-arg NEXUS_PORT=$NEXUS_PORT --no-cache .
+docker build -t $IMAGE --build-arg IMAGE=$IMAGE --build-arg NEXUS_HOST=$NEXUS_HOST --build-arg NEXUS_PORT=$NEXUS_PORT --no-cache .
 docker tag $IMAGE $TARGET
 docker login $DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT -u $DOCKER_PRIVATE_REGISTRY_USER -p $DOCKER_PRIVATE_REGISTRY_PASSWORD
 
