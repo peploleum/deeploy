7ad780d45ac0bd8091608e7df8bbfab0d54522a5
peploleum <peploleum@gmail.com>
2019-05-20 11:57:52 +0200

    updated custom build based on repository requirements

--- docker/registry/python/custom/build-image.sh
+++ docker/registry/python/custom/build-image.sh
@@ -3,19 +3,20 @@
 . ../../docker.conf
 . ../nexus.conf
 
-export IMAGE=python:3.6.8-alpine3.9
+# change base image here  (3.6.8-jessie | 3.6.8-alpine3.8)
+export IMAGE=3.6.8-jessie
 export SOURCE=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PULL_PORT/$IMAGE
 export TARGET=$DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT/$IMAGE
 
-# Update pip.conf file
-sed -i -e "s/##NEXUS_HOST##/$NEXUS_HOST/g" pip.conf
-sed -i -e "s/##NEXUS_PORT##/$NEXUS_PORT/g" pip.conf
-
 echo $SOURCE
 echo $TARGET
-docker build -t $IMAGE --build-arg IMAGE=$SOURCE --build-arg NEXUS_HOST=$NEXUS_HOST --build-arg NEXUS_PORT=$NEXUS_PORT --no-cache .
-docker tag $IMAGE $TARGET
-docker login $DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT -u $DOCKER_PRIVATE_REGISTRY_USER -p $DOCKER_PRIVATE_REGISTRY_PASSWORD
-
-echo pushing $TARGET
-docker push $TARGET
+(docker build -t $IMAGE --build-arg IMAGE=$IMAGE --build-arg NEXUS_HOST=$NEXUS_HOST --build-arg NEXUS_PORT=$NEXUS_PORT --no-cache .)
+if [ "$?" == 0 ] ; then
+  docker tag $IMAGE $TARGET
+  docker login $DOCKER_PRIVATE_REGISTRY_HOST:$DOCKER_PRIVATE_REGISTRY_PUSH_PORT -u $DOCKER_PRIVATE_REGISTRY_USER -p $DOCKER_PRIVATE_REGISTRY_PASSWORD
+  echo pushing $TARGET
+  docker push $TARGET
+else
+  echo "failed to build image"
+	exit
+fi
